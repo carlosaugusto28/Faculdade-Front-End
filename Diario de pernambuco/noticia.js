754c7bf2d67bd01551609ebbcb4b3e900df67811
carlosaugusto28 <109244715+carlosaugusto28@users.noreply.github.com>
2023-05-07 22:44:56 -0300

    Projetos Frond-end

--- Diario de pernambuco/noticia.js
+++
@@ -1,9 +0,0 @@
-function irlogin(){
-    window.location.href = "login.html"
-}
-function pedirNome(){
-    var nome = prompt("Olá, qual é o seu nome?")
-    if (nome!= null && nome!= ""){
-        alert("Bem vindo, " + nome + "!")
-    }
-}
